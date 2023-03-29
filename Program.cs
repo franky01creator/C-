d9fb12054fc0962f2c68b58b20b25cd1f000505d
franky01creator <67398031+franky01creator@users.noreply.github.com>
2023-03-29 17:31:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+
+            if (num < 5)
+            {
+                Console.WriteLine("The number is less than 5");
+            }
+            else if (num >= 5 && num < 10)
+            {
+                Console.WriteLine("The number is between 5 and 10");
+            }
+            else
+            {
+                Console.WriteLine("The number is greater than or equal to 10");
+            }
+        }
+    }
+}
